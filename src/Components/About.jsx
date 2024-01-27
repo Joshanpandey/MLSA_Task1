4f5b6bc48c3619cee79180eaee6a591b0afdbd7a
Joshan Pandey <joshan.pandey@gmail.com>
2024-01-27 23:01:00 +1100

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "abstract background with beautiful color";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Master's Degree student studying Business Information Systems at AIH in Sydney , Australia. I am keenly interested in coding and developing new projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Aspiring Web Developer",
+  "Java Programming Language",
+  "Data Structure and ALgorithms",
+  "Python Programming Language",
+  
 ];
 
 /**
